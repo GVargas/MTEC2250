7d095251500343e871a155e0f8614695a7735a4e
GVargas <gino2229@live.com>
2016-05-11 15:18:34 -0400

    Photo Interrupter Code

---
+++ Photo_Interrupter_Code.ino
@@ -0,0 +1,17 @@
+int sensorRead = 1;
+
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(9, OUTPUT);
+}
+
+void loop() {
+  sensorRead = digitalRead(2);
+
+  if (sensorRead > 0) {
+    digitalWrite(9, HIGH);
+  }
+  else {
+    digitalWrite(9, LOW);
+  }
+}
